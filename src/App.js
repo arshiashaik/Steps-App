1a8044e92f7644d6439fabbc2bfd4c36c87c4424
arshiashaik <shaikarshiazainab@gmail.com>
2024-02-15 20:37:08 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
+import { useState } from "react";
+
+const messages = ["Learn React", "Make Projects", "Apply for Jobs"];
+
 export default function App() {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+
+  const [isOpen, setisOpen] = useState("true");
+
   return (
-    <div className="main-container">
-      <div className="numbers-container">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
+    <div>
+      <button
+        className="close"
+        onClick={() => {
+          setisOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div>
+          <h1>Steps to Become a React Developer</h1>
+          <div className="main-container">
+            <div className="numbers-container">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
 
-      <p className="active-step-message"></p>
+            <p className="active-step-message">
+              Step {step}: {messages[step - 1]}
+            </p>
 
-      <div className="buttons-conatiner">
-        <button>Previous</button>
-        <button>Next</button>
-      </div>
+            <div className="buttons-conatiner">
+              <button onClick={handlePrevious}>Previous</button>
+              <button onClick={handleNext}>Next</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
